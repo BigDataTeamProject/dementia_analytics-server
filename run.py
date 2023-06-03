6b80c307d9d3073c6f926af59b7d086f9e91b044
Jeonhui <dlwjsgml02@naver.com>
2023-06-03 16:57:00 +0900

    feat: add Model

--- run.py
+++ run.py
@@ -1,24 +1,46 @@
 from flask import Flask, jsonify, request
 import numpy as np
-import pickle
+import joblib
+
+import os
+
+print(os.path.dirname(os.path.realpath(__file__)))
+os.chdir("/Users/ijeonhui/Github/BigDataTeamProject/dementia_analytics-server")
 
 app = Flask(__name__)
 
 # 모델 로드
-with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
+with open('./xgb_model.pkl', 'rb') as f:
+    loaded_scaler = joblib.load('xgb_scaler.pkl')
+    loaded_model = joblib.load('xgb_model.pkl')
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()  # POST 요청에서 딕셔너리 데이터 가져오기
     features = preprocess_data(data)  # 데이터 전처리 함수 호출
-    prediction = model.predict(features)  # 예측
+    prediction = loaded_model.predict(loaded_scaler.transform(features))  # 예측
+    print(prediction)
     result = {'prediction': prediction.tolist()}
     return jsonify(result)
 
+
 def preprocess_data(data):
-    features = np.array(list(data.values()))
+    features = np.array([[
+        data['sleep_breath_average'],
+        data['sleep_hr_average'],
+        data['sleep_hr_lowest'],
+        data['sleep_deep'],
+        data['sleep_rem'],
+        data['activity_cal_total'],
+        data['sleep_awake'],
+        data['activity_steps'],
+        data['activity_total'],
+        data['sleep_duration'],
+        data['activity_daily_movement']
+    ]])
     return features
+
 
 if __name__ == '__main__':
     app.run()
