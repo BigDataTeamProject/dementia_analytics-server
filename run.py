118e34eb601f9a9cae233b2280f21d8a9ece9530
sungrokgi <99236118+sungrokgi@users.noreply.github.com>
2023-06-04 19:23:09 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request
 import numpy as np
+import pandas as pd
 import joblib
-
+import csv
 import os
 
 print(os.path.dirname(os.path.realpath(__file__)))
@@ -15,13 +16,14 @@ with open('./xgb_model.pkl', 'rb') as f:
     loaded_model = joblib.load('xgb_model.pkl')
 
 
+dataset_mean = pd.read_csv('파일위치/dataset_mean.csv', encoding = 'utf-8')
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()  # POST 요청에서 딕셔너리 데이터 가져오기
     features = preprocess_data(data)  # 데이터 전처리 함수 호출
     prediction = loaded_model.predict(loaded_scaler.transform(features))  # 예측
     print(prediction)
-    result = {'prediction': prediction.tolist()}
+    result = {'prediction': prediction.tolist,'mean': dataset_mean}
     return jsonify(result)
 
 
@@ -41,6 +43,16 @@ def preprocess_data(data):
     ]])
     return features
 
+# dataset_mean = pd.read_csv('파일위치/dataset_mean.csv', encoding = 'utf-8')
+# def load_dataset_mean(dataset_mean.csv):
+#     dataset_mean = {}
+#     with open(dataset_mean.csv, 'r') as csv_file:
+#         reader = csv.DictReader(csv_file)
+#         for row in reader:
+#             feature_name = row['Feature']
+#             mean_value = float(row['Mean'])
+#             dataset_mean[feature_name] = mean_value
+#     return dataset_mean
 
 if __name__ == '__main__':
     app.run()
